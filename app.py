f11c8b9be13415d8d558d2de73fe1b10127620c0
Varigarble <58060789+Varigarble@users.noreply.github.com>
2021-01-25 20:50:50 -0800

    redis testing
    printing variable assignments for Heroku logs

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def create_app():
     @app.route("/chess-input-1", methods=['GET', 'POST'])
     def input_page_1():
         nonlocal piece
+        print("page 1 load piece: ", piece)
         message = None
         valid = 0
         if request.method == "POST":
@@ -48,7 +49,7 @@ def create_app():
             # piece = session["piece"]
             r.set('piece', message_piece[1])
             piece = r.get('piece')
-            print(piece)  # del
+            print("page 1 set piece: ", piece)  # del
             valid = message_piece[2]
         if valid != 1:
             valid = "<p>Follow instructions to continue.</p>"
@@ -65,8 +66,10 @@ def create_app():
     @app.route("/chess-input-2", methods=['GET', 'POST'])
     def input_page_2():
         nonlocal color
+        print("page 2 load color: ", color)
         # piece = session["piece"]
         piece = r.get('piece')
+        print("page 2 load piece: ", piece)
         message = "Color Input:"
         valid = 0
         if request.method == "POST":
@@ -76,6 +79,7 @@ def create_app():
             # color = session["color"]
             r.set('color', message_color[1])
             color = r.get('color')
+            print("page 2 set color: ", color)
             valid = message_color[2]
         if valid != 1:
             valid = "<p>Follow instructions to continue.</p>"
@@ -93,10 +97,13 @@ def create_app():
     @app.route("/chess-input-3", methods=['GET', 'POST'])
     def input_page_3():
         nonlocal row
+        print("page 3 load row: ", row)
         # piece = session["piece"]
         # color = session["color"]
         piece = r.get('piece')
+        print("page 3 load piece: ", piece)
         color = r.get('color')
+        print("page 3 load color: ", color)
         message = "Row Input:"
         valid = 0
         if request.method == "POST":
@@ -106,6 +113,7 @@ def create_app():
             # row = session["row"]
             r.set('row', message_row[1])
             row = r.get('row')
+            print("page 3 set row: ", row)
             valid = message_row[2]
         if valid != 1:
             valid = "<p>Follow instructions to continue.</p>"
@@ -125,12 +133,17 @@ def create_app():
     def input_page_4():
         nonlocal column
         nonlocal position
+        print("page 4 load column: ", column)
+        print("page 4 load position: ", position)
         # piece = session["piece"]
         # color = session["color"]
         # row = session["row"]
         piece = r.get('piece')
+        print("page 4 load piece: ", piece)
         color = r.get('color')
+        print("page 4 load color: ", color)
         row = r.get('row')
+        print("page 4 load row: ", row)
         message = "Column Input:"
         valid = 0
         if request.method == "POST":
@@ -140,10 +153,12 @@ def create_app():
             # column = session["column"]
             r.set('column', message_col[1])
             column = r.get('column')
+            print("page 4 set column: ", column)
             # session["position"] = message_col[2]
             # position = session["position"]
             r.set('position', message_col[2])
             position = r.get('position')
+            print("page 4 set position: ", position)
             valid = message_col[3]
         if valid != 1:
             valid = "<p>Follow instructions to continue.</p>"
@@ -196,9 +211,11 @@ def create_app():
 
     @app.route("/session_clear")
     def clear():
-        print(session)
-        session.clear()
-        print(session)  # verify session cleared
+        # print(session)
+        # session.clear()
+        # print(session)  # verify session cleared
+        r.delete("piece", "color", "row", "column", "position")
+        r.print("piece", "color", "row", "column", "position")
         piece, color, row, column, position = None, None, None, None, None
         print(piece, color, row, column, position)  # verify variables cleared
         return redirect(url_for("entry_page"))
